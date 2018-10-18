1e00cb2b5bd1c2b00931fc400321d16977fd1c33
AlonsoGD <dani.alonso.garcia@gmail.com>
2018-10-17 21:29:50 -0400

    git ignore

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const express = require('express');
-const app = express();
+const http = require('http');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const hostname = '127.0.0.1';
+const port = 3000;
 
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('<div>');
+});
 
-app.listen(3000, () =>
-  console.log('Server Ready'))
-
-
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
